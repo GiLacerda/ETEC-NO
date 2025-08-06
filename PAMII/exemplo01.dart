8bb176e0f9ceae80ac08686674f5c5a6b94ca95e
Giovanna Lacerda <93397396+GiLacerda@users.noreply.github.com>
2025-08-05 23:22:30 -0300

    Create exemplo01.dart

---
+++ PAMII/exemplo01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Hello World')),
+        body: const Center(child: Text('Olá, Mundo!')),
+      ),
+    ),
+  );
+}
